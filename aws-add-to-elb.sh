687f376ac83588f0b4fb6817731887608cfbbb28
Volkov Volodymyr <vovo@ask4ua.com>
2019-07-18 03:13:16 +0300

    adding some vars to aws-add-to-elb

--- aws-add-to-elb.sh
+++ aws-add-to-elb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/aws elbv2 register-targets --target-group-arn arn:aws:elasticloadbalancing:eu-central-1:168128199012:targetgroup/JenkinsHttpServers/8b3b02c604e05664  --targets Id=${1}
+/usr/bin/aws elbv2 register-targets --region ${3}  --target-group-arn ${2}  --targets Id=${1}
