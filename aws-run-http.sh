2ba83e18b857f236469f586ef6ed9a5a2b73d0c3
Volkov Volodymyr <vovo@ask4ua.com>
2019-07-18 10:30:25 +0300

    just renamed HTTP

--- aws-run-http.sh
+++ aws-run-http.sh
@@ -6,4 +6,4 @@
 #$4 - security group
 #$5 - subnet id
 
-/usr/bin/aws ec2 run-instances --image-id ${1} --count 1 --instance-type 't2.micro' --key-name ${2} --region ${3} --security-group-id ${4} --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=http11-student18},{Key=Owner,Value=student18},{Key=scope,Value=jenkins}]' 'ResourceType=volume,Tags=[{Key=Name,Value=http11-student18},{Key=Owner,Value=student18}]' --subnet-id ${5} --user-data 'file://deploy-httpd.txt' | grep InstanceId | awk -F\" '{print $4}'
+/usr/bin/aws ec2 run-instances --image-id ${1} --count 1 --instance-type 't2.micro' --key-name ${2} --region ${3} --security-group-id ${4} --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=HTTP-student18},{Key=Owner,Value=student18},{Key=scope,Value=jenkins}]' 'ResourceType=volume,Tags=[{Key=Name,Value=http11-student18},{Key=Owner,Value=student18}]' --subnet-id ${5} --user-data 'file://deploy-httpd.txt' | grep InstanceId | awk -F\" '{print $4}'
