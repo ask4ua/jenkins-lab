94d46eb18a4051bdc96ac81f6f2cc86076dc07cd
Volkov Volodymyr <vovo@ask4ua.com>
2019-07-18 10:53:40 +0300

    save

--- aws-add-to-elb.sh
+++ aws-add-to-elb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/aws elbv2 register-targets --region ${3}  --target-group-arn ${2}  --targets Id=${1}
+/usr/bin/aws elbv2 register-targets --targets Id=${1} --region ${2}  --target-group-arn ${3}
